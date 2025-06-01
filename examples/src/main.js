1b13e4bf71f89516ddf372e80df41407141837ee
samyou <>
2025-06-01 20:21:37 +0800

    feat: example库

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { EButton } from '../../packages'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(EButton).mount('#app')
